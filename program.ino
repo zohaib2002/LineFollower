0a2dd1a61f0f5e1bffc7a3694ec648be0668a7c9
Zohaib Hamdule <68106969+zohaib2002@users.noreply.github.com>
2021-11-15 16:54:59 +0300

    Add files via upload

---
+++ program.ino
@@ -0,0 +1,45 @@
+#include<AFMotor.h>
+
+int LS = A4;
+int RS = A5;
+
+AF_DCMotor motor1(4); //sag motor
+AF_DCMotor motor2(3); //sol motor //left
+
+int LSVAL = 0;
+int RSVAL = 0;
+int threshold = 500;
+
+void setup() {
+
+  motor1.setSpeed(155);
+  motor2.setSpeed(155);
+
+}
+
+void loop() {
+
+  LSVAL = analogRead(LS);
+  RSVAL = analogRead(RS);
+
+  if (LSVAL < threshold && RSVAL < threshold) {
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+  }
+
+  if (LSVAL > threshold && RSVAL > threshold) {
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+  }
+
+  if (LSVAL > threshold && RSVAL < threshold) {
+    motor1.run(FORWARD);
+    motor2.run(BACKWARD);
+  }
+
+  if (LSVAL < threshold && RSVAL > threshold) {
+    motor1.run(BACKWARD);
+    motor2.run(FORWARD);
+  }
+
+}
